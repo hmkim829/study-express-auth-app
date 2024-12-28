7dd55346f60f2e432abbacf1868fc38f263a029e
hmkim829 <hmkim829@gmail.com>
2024-12-29 01:21:49 +0900

    feat: mongoDB 연결

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+mongoose.connect(`mongodb://root:1234@localhost:27017`)
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.error(err);
+    })
 
 const port = 4000;
 app.listen(port, () => {
