4a8f3ff611b931f43ab3518386479a104ceed79d
hmkim829 <hmkim829@gmail.com>
2024-12-29 01:24:05 +0900

    feat: 정적파일 제공을 위한 미들웨어 설정

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const path = require("node:path");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(`mongodb://root:1234@localhost:27017`)
     .then(() => {
