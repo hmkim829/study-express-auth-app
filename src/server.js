77c6c9b9de9d875bce6de67d686acc6875a70722
hmkim829 <hmkim829@gmail.com>
2024-12-29 00:45:12 +0900

    feat: 프로젝트 셋팅

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
