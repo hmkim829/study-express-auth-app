5fa8c6c3d6adb6e91af75c2825674a263cf98d36
hmkim829 <hmkim829@gmail.com>
2024-12-29 23:48:46 +0900

    feat: view engine ejs로 셋팅

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
+// view engine configuration
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 mongoose.connect(`mongodb://root:1234@localhost:27017`)
     .then(() => {
         console.log("MongoDB Connected");
